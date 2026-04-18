b7b9a7b7d5cbd8b2a9f306c43fe6ab778516362a
Dang Trung Hieu <94620323+Dangtrunghieu2002@users.noreply.github.com>
2026-04-19 00:14:03 +0700

    reverse stg_customers

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -2,12 +2,12 @@
 
 SELECT 
     "CustomerKey" AS customer_id,
-    "Customer_ID" AS customer_alt_id,
+    "Customer ID" AS customer_alt_id,
     "Customer" AS full_name,
     "City" AS city_name,
-    "State_Province" AS state_province,
-    "Country_Region" AS country_region,
-    "Postal_Code" AS zip_code
+    "State-Province" AS state_province,
+    "Country-Region" AS country_region,
+    "Postal Code" AS zip_code
 FROM {{ source('adventureworks', 'Customers') }}
 -- Filter out the "Not Applicable" placeholder for cleaner analysis
 WHERE "CustomerKey" <> -1
